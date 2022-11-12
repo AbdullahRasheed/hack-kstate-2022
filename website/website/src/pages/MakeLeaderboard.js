ba290cf18d12b6024a91f8b1a9aed93e666c0207
JohnHill42 <giovannipc42@gmail.com>
2022-11-12 15:01:23 -0600

    makeleaderboard styling

--- website/website/src/pages/MakeLeaderboard.js
+++ website/website/src/pages/MakeLeaderboard.js
@@ -1,5 +1,40 @@
-function MakeLeaderboard() {
-    return(<h1>make leaderboard</h1>);
-}
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
 
-export default MakeLeaderboard
+function MakeLeaderboard() {
+    return (
+      <Form>
+        <Form.Group className="m-5" controlId="formBasicUsername">
+            <Form.Label>Make a Leaderboard</Form.Label>
+            <FloatingLabel
+                controlId="floatingInput"
+                label="Name"
+                className="mt-3"
+                >
+                <Form.Control type="username" placeholder="Username"/>
+            </FloatingLabel>
+            <FloatingLabel
+                controlId="floatingInput"
+                label="Category"
+                className="mt-3"
+                >
+                <Form.Control type="username" placeholder="Username"/>
+            </FloatingLabel>
+            <FloatingLabel 
+                controlId="floatingSelect" 
+                label="Leaderboard Types"
+                className="mt-3"
+                >
+                <Form.Select aria-label="Floating type">
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </Form.Select>
+            </FloatingLabel>
+        </Form.Group>
+      </Form>
+    );
+  }
+  
+  export default MakeLeaderboard;

9685d90595938e6929ad2453767aa8fad953a537
DeDose1 <ahrasheed22@ksu.edu>
2022-11-11 22:45:03 -0600

    User service base

---
+++ WebApi/user-service/Controllers/LoginController.cs
@@ -0,0 +1,6 @@
+﻿namespace user_service.Controllers
+{
+    public class LoginController
+    {
+    }
+}
---
+++ WebApi/user-service/Data/UserLogin.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace user_service.Data
+{
+    public class UserLogin
+    {
+
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; } = new byte[0];
+
+        public byte[] PasswordSalt { get; set; } = new byte[0];
+    }
+}

46887a2d4f115b1bc38e1f03449a882140c7ee67
AbdullahRasheed <ahrasheed2004@gmail.com>
2022-11-11 23:11:38 -0600

    Leaderboard Mongo

--- WebApi/leaderboard-service/User_DataBase/BsonIDAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace leaderboard_service.User_DataBase
-{
-    internal class BsonIDAttribute : Attribute
-    {
-    }
-}
--- WebApi/user-service/Controllers/LoginController.cs
+++ WebApi/user-service/Controllers/LoginController.cs
@@ -1,6 +1,18 @@
-﻿namespace user_service.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using user_service.Data;
+
+namespace user_service.Controllers
 {
+    [ApiController]
+    [Route("auth")]
     public class LoginController
     {
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser([FromBody] LoginCredentials creds)
+        {
+
+        }
+
     }
 }

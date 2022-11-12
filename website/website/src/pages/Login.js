4a35b9029df86611eddb20e8d5994a2d0fa20485
JohnHill42 <giovannipc42@gmail.com>
2022-11-12 14:45:31 -0600

    login styling

--- website/website/src/pages/Login.js
+++ website/website/src/pages/Login.js
@@ -1,26 +1,39 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import './Login.css'
 
 function Login() {
   return (
     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+      <Form.Group className="m-5" controlId="formBasicUsername">
+        <Form.Label>Login</Form.Label>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Username"
+            className="mt-3"
+          >
+            <Form.Control type="username" placeholder="Username"/>
+        </FloatingLabel>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="m-5" controlId="formBasicPassword">
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Password"
+          className="mb-3"
+        >
+          <Form.Control type="password" placeholder="Password" />
+        </FloatingLabel>
+        <Form.Text className="text-muted">
+        Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji
+        </Form.Text>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Stay logged in" />
+      <Form.Group className="mx-5" controlId="formBasicCheckbox">
+        <Form.Check type="switch" label="Stay logged in" />
       </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
+      <Button className="mx-5" variant="primary" type="submit" >
+        Login
       </Button>
     </Form>
   );

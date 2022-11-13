daaa4730681f351d272371a7bb04e996e528a038
JohnHill42 <giovannipc42@gmail.com>
2022-11-13 03:06:18 -0600

    front

--- website/website/src/pages/Homepage.js
+++ website/website/src/pages/Homepage.js
@@ -2,17 +2,38 @@ import './Homepage.css';
 import Button from 'react-bootstrap/esm/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/esm/Container';
+import { useNavigate } from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
+import Figure from 'react-bootstrap/Figure';
 
 const Homepage = () => {
+    const navigate = useNavigate();
+    const signUpClick = () => {
+        navigate('/signup')
+    }
+    const loginClick = () => {
+        navigate('/login')
+    } 
     return (
         <div className='Homepage'>
             <h1 className='Homepage-header'>
                 Welcome to LeadTheBoard
             </h1>
-            <p className='Homepage-p'>  
-                Hello
-            </p> 
-            <Button></Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <Button variant="primary" onClick={signUpClick} size='lg'>
+                            Sign Up
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button variant="primary" onClick={loginClick} size='lg'>
+                            Login
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
   };
